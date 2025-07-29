a7019016957859fe9aa52074754622b2b8cff7d0
SuzukiKouji <suzuki.kouji@tokyo-gas.co.jp>
2025-07-29 20:49:27 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
-const date = new Date('2024-10-12');
+const date = new Date();
 const year = date.getFullYear();
 const month = date.getMonth() + 1; // 月は0から始まるので +1
 const day = date.getDate();
 
-console.log(`今日の日付は ${year}年${month}月${day}日です。`);
+console.log(”{year}年${month}月${day}”);
