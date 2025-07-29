92d70c71aeabaa7f840a3722ba191c818e1f5e0d
SuzukiKouji <suzuki.kouji@tokyo-gas.co.jp>
2025-07-29 20:56:30 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,6 +0,0 @@
-const date = new Date();
-const year = date.getFullYear();
-const month = date.getMonth() + 1; // 月は0から始まるので +1
-const day = date.getDate();
-
-console.log(”{year}年${month}月${day}”);
